fd40d7a51a88470c0fe0f721ca90bdd0546b9224
Daniil Kukhar <82108121+DaniilKukhar@users.noreply.github.com>
2022-12-07 14:30:23 +0200

    Add project files

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Danya Kukhar on 07.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
