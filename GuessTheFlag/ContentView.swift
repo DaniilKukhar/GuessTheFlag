a5f014588892bc6a2ae3f2384c4797d96ef795d5
Daniil Kukhar <82108121+DaniilKukhar@users.noreply.github.com>
2022-12-07 20:09:11 +0200

    Add alert modifier

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France",
+    @State private var scoreTitle = ""
+    @State private var showingScore = false
+    @State private var scoreAmount: Int = 0 {
+        willSet {
+            if newValue == -1 {
+                scoreAmount = 0
+            }
+        }
+    }
+    
+    @State var countries = ["Estonia", "France",
                      "Germany", "Ireland",
                      "Italy", "Monaco",
                      "Nigeria", "Poland",
-                     "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+                     "Spain", "UK", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
@@ -29,7 +39,7 @@ struct ContentView: View {
                 }
                 ForEach(0..<3) { number in
                     Button {
-                        
+                       flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -37,6 +47,28 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("You are score is \(scoreAmount)")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            scoreAmount += 1
+        } else {
+            scoreTitle = "Wrong"
+            scoreAmount -= 1
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
